9e47b5aec77acb7dbc83806a815cb58f591bed2b
Hansel Perez <hanselperez1@gmail.com>
2018-01-17 08:07:46 -0500

    Added collision actions.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // okay to include as long as this is the only script that loads scenes
+
+public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+        deathFX.SetActive(true);
+    }
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; // Step 1: Allow this functionality
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float ControlSpeed = 30f;
+    [Tooltip("In m")] [SerializeField] float xRange = 15f;
+    [Tooltip("In m")] [SerializeField] float yRange = 10f;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float positionPitchFactor = .3f;
+    [SerializeField] float positionYawFactor = 3f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float controlPitchFactor = 3f;
+    [SerializeField] float controlRollFactor = -50f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+        ProcessTranlation();
+        ProcessRotation();
+        }
+    }
+
+    void OnPlayerDeath() // called by string reference
+    {
+        isControlEnabled = false;
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranlation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * ControlSpeed * Time.deltaTime;
+        float yOffset = yThrow * ControlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
