9e087b0de428c016e29234f6e5df2d319f0b0fce
Hansel Perez <hanselperez1@gmail.com>
2018-01-18 12:07:26 -0500

    Fixed inconsistency issue with destroying obstacle.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
+
 
 	// Use this for initialization
 	void Start () {
