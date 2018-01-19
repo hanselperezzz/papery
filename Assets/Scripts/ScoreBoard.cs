cf0509f93b33d05fbb2c18f9debb73a86f07ec71
Hansel Perez <hanselperez1@gmail.com>
2018-01-19 13:01:24 -0500

    Trying to figure out stash.

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -19,7 +19,6 @@ public class ScoreBoard : MonoBehaviour {
 	
 	public void ScoreHit(int scscoreIncrease)
     {
-        //change 1
         score = score + scscoreIncrease;
         scoreText.text = score.ToString();
     }
